bd5ea93040889a6cc024fe845997129d37079da7
narteysarso <narteysarso@gmail.com>
2020-04-16 13:27:13 +0000

    resolved sls error

--- backend/utils.js
+++ backend/utils.js
@@ -146,10 +146,9 @@ async function mail({from="",to, subject="",text="",html=""}) {
   try {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-      // host: process.env.MAIL_HOST,
-      // port: process.env.MAIL_PORT,
+      
       service: 'gmail',
-      // secure: process.env.SECURE_MAIL, // true for 465, false for other ports
+      
       auth: {
         user: `${process.env.MAIL_USERNAME}`, // generated ethereal user
         pass: `${process.env.MAIL_PASSWORD}` // generated ethereal password
